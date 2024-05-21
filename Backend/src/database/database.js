373638f88c378e7c58ba9d9dfcc4f287c1b013fc
LCRMTZ <luccraft13@gmail.com>
2024-05-20 20:49:54 -0600

    ..

--- Backend/src/database/database.js
+++ Backend/src/database/database.js
@@ -1,23 +1,21 @@
-import mysql from 'mysql2/promise'; // Importa el paquete 'mysql2/promise' para interactuar con la base de datos MySQL
-import config from '../config'; // Importa la configuración de la base de datos desde el archivo de configuración
+const mysql = require('mysql2/promise');
+const config = require('../config');
 
-// Configuración de las credenciales y detalles de conexión a la base de datos
 const dbsettings = {
-    host: config.dbServer, // Servidor de la base de datos
-    user: config.dbUser, // Nombre de usuario para acceder a la base de datos
-    password: config.dbPassword, // Contraseña para el usuario de la base de datos
-    database: config.dbDatabase, // Nombre de la base de datos
+    host: config.dbServer,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbDatabase,
 };
 
-// Función para establecer la conexión a la base de datos
-export async function getConnection() {
+async function getConnection() {
     try {
-        const connection = await mysql.createConnection(dbsettings); // Crea una conexión usando la configuración definida
-        console.log("Connected to the database!"); // Mensaje de éxito
+        const connection = await mysql.createConnection(dbsettings);
+        console.log("Connected to the database!");
         return connection;
     } catch (error) {
-        console.error("Error connecting to the database: ", error); // Muestra el error en caso de fallo
+        console.error("Error connecting to the database: ", error);
     }
 }
 
-export { mysql }; // Exporta el módulo 'mysql' para ser utilizado en otras partes del código
+module.exports = { getConnection, mysql };
